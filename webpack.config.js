ec3e6f4b82a723c34dd4e881a7ac21525d693fd5
Andrey Esin <andrey@appliedlabs.io>
2020-12-02 09:55:17 +0300

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,6 +8,12 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Thread comments',
+    }),
+  ],
   module: {
     rules: [
       {
